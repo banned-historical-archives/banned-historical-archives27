8451a40bc4851aefe5bdb9e3ffd306d41ff9f465
Your Name <you@example.com>
2024-08-10 21:31:57 +0800

    1

---
+++ 177dc947-4213-43e3-825e-459076e353ac.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "177dc947-4213-43e3-825e-459076e353ac",
+    "name": "贺贤春同志在杭州市工代会四月二日会议上的讲话",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "贺贤春",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives27/main/177dc947-4213-43e3-825e-459076e353ac/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives27/main/177dc947-4213-43e3-825e-459076e353ac/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives27/main/177dc947-4213-43e3-825e-459076e353ac/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives27/main/177dc947-4213-43e3-825e-459076e353ac/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives27/main/177dc947-4213-43e3-825e-459076e353ac/5.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "贺贤春同志在杭州市工代会四月二日会议上的讲话",
+        "authors": [
+          "贺贤春"
+        ],
+        "page_start": 1,
+        "page_end": 5,
+        "dates": [
+          {
+            "year": 1974,
+            "month": 4,
+            "day": 2
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "177dc947-4213-43e3-825e-459076e353ac",
+  "resource_type": "book",
+  "version": 2
+}
